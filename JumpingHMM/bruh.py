05375490be2301e4aefe726c7071448a8911969b
mayukh <dasmayukh2002@gmail.com>
2024-11-19 16:19:00 -0800

    Jumping HMM Progress

--- JumpingHMM/bruh.py
+++ JumpingHMM/bruh.py
@@ -1,32 +1,32 @@
-states = ["M1", "M2", "M3", "M4", "M5", "I1", "I2", "I3", "I4"]
-N = len(states)
-state_id = {states[idx]: idx for idx in range(N)}
-
-mat = [[0 for _ in range(N)] for _ in range(N)]
-
-mat[state_id["M1"]][state_id["M2"]] = 0.9
-mat[state_id["M2"]][state_id["M3"]] = 0.9
-mat[state_id["M3"]][state_id["M4"]] = 0.9
-mat[state_id["M4"]][state_id["M5"]] = 0.9
-mat[state_id["M5"]][state_id["M5"]] = 1
-
-mat[state_id["I1"]][state_id["I1"]] = 0.4
-mat[state_id["I2"]][state_id["I2"]] = 0.4
-mat[state_id["I3"]][state_id["I3"]] = 0.4
-mat[state_id["I4"]][state_id["I4"]] = 0.4
-
-emission_matrix = [
-    [0.7, 0.1, 0.1, 0.1, ]
-    [0.1, 0.1, 0.7, 0.1,]
-    [0.1, 0.8, 0.1, 0,]
-    [0.1, 0.1, 0.1, 0.7,]
-    [0.8, 0, 0, 0.2,]
-    [0.25, 0.25, 0.25, 0.25,]
-    [0.25, 0.25, 0.25, 0.25,]
-    [0.25, 0.25, 0.25, 0.25,]
-    [0.25, 0.25, 0.25, 0.25]
-]
-start_probs = [0.2, 0.2, 0.2, 0.2, 0.2, 0, 0, 0, 0]
-
-for row in mat:
-    print(row)
+# states = ["M1", "M2", "M3", "M4", "M5", "I1", "I2", "I3", "I4"]
+# N = len(states)
+# state_id = {states[idx]: idx for idx in range(N)}
+#
+# mat = [[0 for _ in range(N)] for _ in range(N)]
+#
+# mat[state_id["M1"]][state_id["M2"]] = 0.9
+# mat[state_id["M2"]][state_id["M3"]] = 0.9
+# mat[state_id["M3"]][state_id["M4"]] = 0.9
+# mat[state_id["M4"]][state_id["M5"]] = 0.9
+# mat[state_id["M5"]][state_id["M5"]] = 1
+#
+# mat[state_id["I1"]][state_id["I1"]] = 0.4
+# mat[state_id["I2"]][state_id["I2"]] = 0.4
+# mat[state_id["I3"]][state_id["I3"]] = 0.4
+# mat[state_id["I4"]][state_id["I4"]] = 0.4
+#
+# emission_matrix = [
+#     [0.7, 0.1, 0.1, 0.1, ]
+#     [0.1, 0.1, 0.7, 0.1,]
+#     [0.1, 0.8, 0.1, 0,]
+#     [0.1, 0.1, 0.1, 0.7,]
+#     [0.8, 0, 0, 0.2,]
+#     [0.25, 0.25, 0.25, 0.25,]
+#     [0.25, 0.25, 0.25, 0.25,]
+#     [0.25, 0.25, 0.25, 0.25,]
+#     [0.25, 0.25, 0.25, 0.25]
+# ]
+# start_probs = [0.2, 0.2, 0.2, 0.2, 0.2, 0, 0, 0, 0]
+#
+# for row in mat:
+#     print(row)
--- JumpingHMM/otherJumpingHMM.r
+++ JumpingHMM/otherJumpingHMM.r
@@ -1,61 +1,127 @@
-states1 <- c("M1", "M2", "M3", "M4", "M5", "I1", "I2", "I3",
-"I4", "T", "N", "C")
-states2 <- c("m1", "m2", "m3", "m4", "m5", "i1", "i2", "i3",
-"i4")
+states1 <- c("M1", "M2", "M3", "M4", "M5", "I1", "I2", "I3", "I4", "T", "N", "C")
+states2 <- c("m1", "m2", "m3", "m4", "m5", "i1", "i2", "i3","i4")
 states <- c(states1, states2)
 
-transitionMatrixLeftTop <- matrix(data=c(0, 0.9, 0, 0,
-0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0.8, 0, 0, 0, 0.1, 0,
-0, 0, 0, 0, 0, 0, 0, 0.9, 0, 0, 0, 0.1, 0, 0, 0, 0, 0,
-0, 0, 0, 0.9, 0, 0, 0, 0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 1, 0, 0.6, 0, 0, 0, 0.4, 0, 0, 0, 0, 0,
-0, 0, 0, 0.6, 0, 0, 0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0,
-0.6, 0, 0, 0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0.6, 0, 0,
-0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, ?,
-0, 0, 0, 0, 0, 0, 0, 0, 0, 0.8, 0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0.2, 0, 0.8), byrow=TRUE, nrow=12,
+transitionMatrixLeftTop <- matrix(data=c(
+   0, 0.9,   0,   0,   0, 0.1,   0,   0,   0,   0,   0,   0,
+   0,   0, 0.8,   0,   0,   0, 0.1,   0,   0,   0,   0,   0,
+   0,   0,   0, 0.9,   0,   0,   0, 0.1,   0,   0,   0,   0,
+   0,   0,   0,   0, 0.9,   0,   0,   0, 0.1,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,
+   0, 0.6,   0,   0,   0, 0.4,   0,   0,   0,   0,   0,   0,
+   0,   0, 0.6,   0,   0,   0, 0.4,   0,   0,   0,   0,   0,
+   0,   0,   0, 0.6,   0,   0,   0, 0.4,   0,   0,   0,   0,
+   0,   0,   0,   0, 0.6,   0,   0,   0, 0.4,   0,   0,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   0,   0,
+   0.1, 0,   0,   0,   0,   0,   0,   0,   0,   0, 0.8,   0,
+   0,   0,   0,   0,   0,   0,   0,   0,   0, 0.2,   0, 0.8
+   ), byrow=TRUE, nrow=12,
 dimnames=list(states1, states1))
 
 transitionMatrixRightBottom<- matrix(data=c(
-0, 0.9 ,0 , 0, 0 ,0.1, 0, 0, 0, 
-0, 0, 0.1, 0, 0, 0, 0.1, 0, 0, 
-0, 0, 0, 0.9, 0, 0, 0, 0.1, 0,
-0, 0, 0, 0, 0.9, 0, 0, 0, 0.1, 
-0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0.6, 0, 0, 0, 0.4, 0, 0, 0, 0, 0, 0.6, 0, 0, 0,
-0.4, 0, 0, 0, 0, 0, 0.6, 0, 0, 0, 0.4, 0, 0, 0, 0, 0,
-0.6, 0, 0, 0, 0.4), byrow=TRUE, nrow=9,
+   0, 0.9,   0,   0,   0, 0.1,   0,   0,   0,
+   0,   0, 0.8,   0,   0,   0, 0.1,   0,   0,
+   0,   0,   0, 0.9,   0,   0,   0, 0.1,   0,
+   0,   0,   0,   0, 0.9,   0,   0,   0, 0.1,
+   0,   0,   0,   0,   0,   0,   0,   0,   0,
+   0, 0.6,   0,   0,   0, 0.4,   0,   0,   0,
+   0,   0, 0.6,   0,   0,   0, 0.4,   0,   0,
+   0,   0,   0, 0.6,   0,   0,   0, 0.4,   0,
+   0,   0,   0,   0, 0.6,   0,   0,   0, 0.4
+   ), byrow=TRUE, nrow=9,
 dimnames=list(states2, states2))
 
 # a matrix of zeroes
 transitionMatrixRightTop<-matrix(0, length(states1),
 length(states2), dimnames=list(states1, states2))
-transitionMatrixRightTop [?, 3]<-0.1
+transitionMatrixRightTop [2, 3]<-0.1
 transitionMatrixRightTop [11, 1]<-0.1
 transitionMatrixLeftBottom<-matrix(0, length(states2),
 length(states1), dimnames=list(states2, states1))
 transitionMatrixLeftBottom [2, 3]<- 0.8
 transitionMatrixLeftBottom [5, 12]<-1
 
+
+# Combine the transition matrices into a larger matrix for the jumping HMM
+n <- length(states)
+combined_transitionMatrix <- matrix(0, n, n, dimnames=list(states, states))
+
+# Copy intra-HMM transitions
+combined_transitionMatrix[states1, states1] <- transitionMatrixLeftTop
+combined_transitionMatrix[states2, states2] <- transitionMatrixRightBottom
+
+# Add inter-HMM transitions (jumping transitions)
+combined_transitionMatrix[states1, states2] <- transitionMatrixRightTop
+combined_transitionMatrix[states2, states1] <- transitionMatrixLeftBottom
+
 symbols <- c("A", "T", "C", "G", "end")
-emissionMatrix1<- matrix(data=c(0.7, 0.1, 0.1, 0.1, 0,
-0.1, 0.1, 0.7, 0.1, 0, 0.1, 0.8, 0.1, 0, 0, 0.1, 0.1,
-0.1, 0.7, 0, 0.8, 0, 0, 0.2, 0, 0.25, 0.25, 0.25, 0.25,
-0, 0.25, 0.25, 0.25, 0.25, 0, 0.25, 0.25, 0.25, 0.25,
-0,
-0.25, 0.25, 0.25, 0.25, 0, 0, 0, 0, 0, 1, 0.25, 0.25,
-0.25, 0.25, 0, 0.25, 0.25, 0.25, 0.25, 0), byrow=TRUE,
+emissionMatrix1<- matrix(data=c(
+   0.7,   0.1,   0.1,   0.1,     0,
+   0.1,   0.1,   0.7,   0.1,     0,
+   0.1,   0.8,   0.1,     0,     0,
+   0.1,   0.1,   0.1,   0.7,     0,
+   0.8,     0,     0,   0.2,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+     0,     0,     0,     0,     1,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0
+), byrow=TRUE,
 nrow=length(states1), dimnames=list(states1, symbols))
 
-emissionMatrix2<- matrix(data=c(0.4, 0.2, 0.2, 0.2, 0,
-0.2, 0.2, 0.4, 0.2, 0, 0.1, 0.8, 0.1, ?, 0, 0.1, 0, 0,
-0.9, 0, 0.9, 0, 0, 0.1, 0, 0.25, 0.25, 0.25, 0.25, 0,
-0.25, 0.25, 0.25, 0.25, 0, 0.25, 0.25, 0.25, 0.25, 0,
-0.25, 0.25, 0.25, 0.25, 0), byrow=TRUE,
+emissionMatrix2 <- matrix(data=c(
+   0.4,   0.2,   0.2,   0.2,     0,
+   0.2,   0.2,   0.4,   0.2,     0,
+   0.1,   0.8,   0.1,     0,     0,
+   0.1,     0,     0,   0.9,     0,
+   0.9,     0,     0,   0.1,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0,
+  0.25,  0.25,  0.25,  0.25,     0
+), byrow=TRUE,
 nrow=length(states2), dimnames=list(states2, symbols))
 
-emissionMatrix<c(emissionMatrix1, emissionMatrix2)
+emissionMatrix <- rbind(emissionMatrix1, emissionMatrix2)
 
 startProbs <- c(1/12, 1/12, 1/12, 1/12, 1/12, 0, 0, 0,
 0, 0, 1/12, 1/12, 1/12, 1/12, 1/12, 1/12, 1/12, 0, 0,
 0, 0)
+
+jumping_hmm <- initHMM(States = states, 
+                       Symbols = symbols, 
+                       startProbs = startProbs, 
+                       transProbs = combined_transitionMatrix, 
+                       emissionProbs = emissionMatrix)
+
+
+
+# Sequence 1: "GTTTACTGAACACACCC"
+observations <- c("G", "T", "T", "T", "A", "C", "T", "G", "A", "A", "C", "A", "C", "A", "C", "C", "C")
+print('observations <- c("G", "T", "T", "T", "A", "C", "T", "G", "A", "A", "C", "A", "C", "A", "C", "C", "C")')
+viterbi_result <- viterbi(jumping_hmm, observations)
+print(viterbi_result)
+avg_length <- avg_length + length(viterbi_result)
+print("")
+
+# # Sequence 2: "AGCTGA"
+# observations <- c("A", "G", "C", "T", "G", "A")
+# print('observations <- c("A", "G", "C", "T", "G", "A")')
+# viterbi_result <- viterbi(jumping_hmm, observations)
+# print(viterbi_result)
+# avg_length <- avg_length + length(viterbi_result)
+# print("")
+
+# # Sequence 3: "AGCCTGA"
+# observations <- c("A", "G", "C", "C", "T", "G", "A")
+# print('observations <- c("A", "G", "C", "C", "T", "G", "A")')
+# viterbi_result <- viterbi(jumping_hmm, observations)
+# print(viterbi_result)
+# avg_length <- avg_length + length(viterbi_result)
+# print("")
+
+# Calculate and print the average length of Viterbi results
+avg_length <- avg_length / 3
+cat("Average length of Viterbi results:", avg_length, "\n")
